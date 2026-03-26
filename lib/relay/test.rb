af9b5ee638249ce1f21a39c21ef9cf483b912139
Robert <0x1eef@hardenedbsd.org>
2026-03-26 12:38:07 -0300

    Move test setup requires into test/setup.rb

--- lib/relay/test.rb
+++ lib/relay/test.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-require "test/unit"
-require "rack/test"
-
-ENV["RACK_ENV"] = "test"
-
 module Relay
   class Test < Test::Unit::TestCase
     include Rack::Test::Methods
--- test/setup.rb
+++ test/setup.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
+require "test/unit"
+require "rack/test"
+
+ENV["RACK_ENV"] = "test"
+
 require_relative "../app/init"
